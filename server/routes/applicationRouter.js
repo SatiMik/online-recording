443a8b58726e0f9260c310170100c22fab2b5030
SatiMik <sati62638@gmail.com>
2023-10-13 14:21:04 +0300

    applicationUserAdmin

--- server/routes/applicationRouter.js
+++ server/routes/applicationRouter.js
@@ -1,24 +1,47 @@
 const express = require('express');
-const Application = require('../db/models');
+const { Application } = require('../db/models');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const application = await Application.findAll();
-  res.json(application);
-})
-  .post('/', async (req, res) => {
+router
+  // .route('/')
+  .get('/', async (req, res) => {
+    console.log('ЗАШЕЛ ---');
     try {
-      console.log(req.body);
-      const application = await Application.create({
-        phone: req.body.phone,
-        clientName: req.body.clientName,
-      });
-      return res.json(application);
+      const applications = await Application.findAll();
+      res.json(applications);
     } catch (err) {
-      console.log(err, 'ошибка в создании ');
-      return res.sendStatus(400);
+      console.log(err, 'ошибка в получении');
+      res.sendStatus(500);
     }
   });
+router.post('/', async (req, res) => {
+  try {
+    const { phone, clientName } = req.body;
+    console.log(req.body, 'ОТПРАВИЛИ ЗАЯВКУ ------------');
+    const applications = await Application.create({
+      phone,
+      clientName,
+    });
+    res.json(applications);
+  } catch (err) {
+    console.log(err, 'ошибка в создании ');
+    return res.sendStatus(400);
+  }
+});
 
+router
+  .delete('/:id', async (req, res) => {
+    try {
+      const application = await Application.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      return res.sendStatus(200);
+    } catch (err) {
+      console.error(err, 'ошибка в удалении, ----------------');
+      res.sendStatus(500);
+    }
+  });
 module.exports = router;
