874d4a83aa37efcd8383bf41df3f9fa83b92a0b6
DaryaDevyatirikova <nady190277@yandex.ru>
2023-10-14 22:38:02 +0300

    кнопка на запись

--- client/src/components/ui/services/ServiceCard.tsx
+++ client/src/components/ui/services/ServiceCard.tsx
@@ -9,9 +9,10 @@ import {
   Button,
   Box,
   Container,
+  Link,
 } from '@mui/material';
 
-
+import { Link as NavLink } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 
 import ServiceModal from './ServiceModal';
@@ -21,14 +22,13 @@ import { deleteServiceThunk } from '../../../redux/slices/service/ServiceThunks'
 type ServiceCardPropsType = {
   service: ServiceType;
 };
+const linkStyle = { color: 'white', mr: 2, fontFamily: 'Raleway, Arial' };
 
 function ServiceCard({ service }: ServiceCardPropsType): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
   const dispatch = useAppDispatch();
   const user = useAppSelector((store) => store.user);
 
- 
- 
   return (
     <Box mt={8}>
       <Container>
@@ -45,6 +45,9 @@ function ServiceCard({ service }: ServiceCardPropsType): JSX.Element {
             </Typography>
           </CardContent>
           <CardActions>
+            <Link key="record" component={NavLink} to="/online-record" sx={linkStyle}>
+              Записаться
+            </Link>
             {user.status === 'logged' && user?.isAdmin && (
               <>
                 <Button
